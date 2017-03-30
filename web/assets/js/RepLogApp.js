de517e429455da8e3713050ea3f87b8c1fdf4fe1
petrero <petrero@gmail.com>
2017-03-30 13:14:41 +0300

    From now on, we'll pretty much be adding everything to the prototype key. But, it does get a little bit annoying to always need to say Helper.prototype.something = for every method. No worries! We can shorten this with a shortcut that's similar to PHP's array_merge() function. Use {dollar_sign}.extend() and pass it Helper.prototype and then a second object containing all of the properties you want to merge into that object. In other words, move our calculateTotalWeight() function into this and update it to be calculateTotalWeight: function.
    At the bottom, we don't need the semicolon anymore. If we had more properties, we'd add them right below calculateTotalWeight: no need to worry about writing prototype every time.
    
    There's nothing special about {dollar_sign}.extend, it's just a handy array_merge-esque function that we happen to have handy. You may see other functions from other libraries that do the same thing.

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -53,13 +53,15 @@
   Helper.initialize = function($wrapper){
     this.$wrapper = $wrapper;
   };
-  Helper.prototype.calculateTotalWeight = function(){
-    var totalWeight = 0;
-    this.$wrapper.find('tbody tr').each(function(){
-      totalWeight += $(this).data('weight');
-    }); 
-
-    return totalWeight;
-  };
+  
+  $.extend(Helper.prototype, {
+    calculateTotalWeight: function(){
+      var totalWeight = 0;
+      this.$wrapper.find('tbody tr').each(function(){
+        totalWeight += $(this).data('weight');
+      });  
+      return totalWeight;
+    };
+  });
   
 })(window, jQuery);
