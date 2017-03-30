f7e227273815a06796093939daa9fad813e0351d
petrero <petrero@gmail.com>
2017-03-30 13:52:10 +0300

    With this trick, it's super easy to make RepLogApp an instantiatable object. First, set RepLogApp itself to the former initialize() function. I'll un-indent everything and finish it with a semicolon. Constructor done! Next, add {dollar_sign}.extend() with window.RepLogApp.prototype and {. The existing keys fit right into this perfectly! Winning! At the end, add an extra )
    Yes! In our template, we won't use RepLogApp like this anymore. Instead, say var repLogApp = new RepLogApp({dollar_sign}table).
    
    We won't call any methods on that new repLogApp variable, but we could if we wanted to. We could also create multiple RepLogApp objects if we had multiple tables on the page, or if we loaded a table via AJAX. Our JavaScript is starting to be awesome!

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -1,15 +1,15 @@
 'use strict';
 (function(window, $){
-  window.RepLogApp = {
-    initialize: function($wrapper){
-      this.$wrapper = $wrapper; 
-      this.helper = new Helper(this.$wrapper);
+  window.RepLogApp = function($wrapper){
+    this.$wrapper = $wrapper; 
+    this.helper = new Helper(this.$wrapper);
 
-      this.$wrapper.find('.js-delete-rep-log')
-        .on('click', this.handleRepLogDelete.bind(this));
-      this.$wrapper.find('tbody tr')
-        .on('click', this.handleRowClick.bind(this));
-    },
+    this.$wrapper.find('.js-delete-rep-log')
+      .on('click', this.handleRepLogDelete.bind(this));
+    this.$wrapper.find('tbody tr')
+      .on('click', this.handleRowClick.bind(this));
+  };
+  $.extend(window.RepLogApp.prototype, {  
     updateTotalWeightLifted: function(){
       this.$wrapper.find('.js-total-weight')
         .html(this.helper.calculateTotalWeight());
@@ -42,7 +42,7 @@
     handleRowClick: function(){
       console.log('row clicked!');  
     },  
-  };
+  });
 
   /**
   * A "private" object
@@ -50,9 +50,7 @@
   var Helper = function($wrapper){
     this.$wrapper = $wrapper;
   };
-  Helper.initialize = function($wrapper){
-    this.$wrapper = $wrapper;
-  };
+  
   
   $.extend(Helper.prototype, {
     calculateTotalWeight: function(){
@@ -61,7 +59,7 @@
         totalWeight += $(this).data('weight');
       });  
       return totalWeight;
-    };
+    }
   });
   
 })(window, jQuery);
