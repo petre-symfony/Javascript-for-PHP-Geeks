c910c4958faa609a9eaaa0c322679588fb405fea
petrero <petrero@gmail.com>
2017-03-30 12:47:37 +0300

    The whole point of this new constructor and prototype setup is so that we could have multiple instances of our Helper object. The prototype is just the key to take advantage of it.
    To prove it all works, add var helper2 = new Helper() and pass it a different {dollar_sign}wrapper, like the footer on our page.
    
    Since the footer doesn't have any rows that have weight on it, this should return zero. Log that: this.helper.calculateTotalWeight() and helper2.calculateTotalWeight().
    
    Try that! Cool! 157.5 and of course, zero.

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -4,26 +4,16 @@
     initialize: function($wrapper){
       this.$wrapper = $wrapper; 
       this.helper = new Helper(this.$wrapper);
-      
+      var helper2 = new Helper($('footer'));
+      console.log(
+        this.helper.calculateTotalWeight(),
+        helper2.calculateTotalWeight()
+      );
 
       this.$wrapper.find('.js-delete-rep-log')
         .on('click', this.handleRepLogDelete.bind(this));
       this.$wrapper.find('tbody tr')
         .on('click', this.handleRowClick.bind(this));
-      
-      console.log(this.helper, Object.keys(this.helper));
-      console.log(Helper, Object.keys(Helper));
-      console.log(this.helper.calculateTotalWeight());
-      var playObject = {
-        lift: 'stuff'
-      }
-      playObject.__proto__.cat = 'meow';
-      console.log(playObject.lift, playObject.cat);
-      console.log(
-        'foo'.__proto__,
-        [].__proto__,
-        (new Date()).__proto__
-      );
     },
     updateTotalWeightLifted: function(){
       this.$wrapper.find('.js-total-weight')
