1187805ded880014dcd58265922e1f9d09ad8c86
petrero <petrero@gmail.com>
2017-03-31 19:57:55 +0300

    Copy code from branch 24

---
+++ web/assets/js/RepLogApp.js
@@ -0,0 +1,135 @@
+'use strict';
+(function(window, $){
+  window.RepLogApp = function($wrapper){
+    this.$wrapper = $wrapper; 
+    this.helper = new Helper(this.$wrapper);
+
+    this.$wrapper.on(
+      'click',
+      '.js-delete-rep-log',               
+      this.handleRepLogDelete.bind(this)
+    );
+    this.$wrapper.on(
+      'click', 
+      'tbody tr', 
+      this.handleRowClick.bind(this)
+    );
+    this.$wrapper.on(
+      'submit', 
+      this._selectors.newRepForm, 
+      this.handleNewFormSubmit.bind(this)
+    );
+  };
+  $.extend(window.RepLogApp.prototype, {  
+    _selectors: {
+      newRepForm: '.js-new-rep-log-form'
+    },
+    updateTotalWeightLifted: function(){
+      this.$wrapper.find('.js-total-weight')
+        .html(this.helper.calculateTotalWeight());
+    },
+    handleRepLogDelete: function(e){
+      e.preventDefault();
+
+      var $link = $(e.currentTarget);
+      $link.addClass('text-danger');
+      $link
+        .find('.fa')
+        .removeClass('fa-trash')
+        .addClass('fa-spinner')
+        .addClass('fa-spin');
+
+      var deleteUrl = $link.data('url');
+      var $row = $link.closest('tr');
+      var self = this;
+      $.ajax({
+        url: deleteUrl,
+        method: 'DELETE',
+        success: function(){
+          $row.fadeOut('normal', function(){
+            $(this).remove();
+            self.updateTotalWeightLifted();
+          });
+        }
+      }); 
+    },
+    handleRowClick: function(){
+      console.log('row clicked!');  
+    },
+    handleNewFormSubmit: function(e){
+      e.preventDefault();
+      
+      var $form = $(e.currentTarget);
+      var formData = {};
+      var self = this;
+      $.each($form.serializeArray(), function(key, fieldData){
+        formData[fieldData.name] = fieldData.value;  
+      });
+      $.ajax({
+        url: $form.data('url'),
+        method: 'POST',
+        data: JSON.stringify(formData),
+        success: function(data){
+          self._clearForm();
+          self._addRow(data);
+        },
+        error: function(jqXHR){
+          var errorData = JSON.parse(jqXHR.responseText);
+          self._mapErrorsToForm(errorData.errors);
+        }
+      });
+    },
+    _mapErrorsToForm: function(errorData){
+      this._removeFormErrors;
+      var $form = this.$wrapper.find(this._selectors.newRepForm);
+      
+      $form.find(':input').each(function(){
+        var fieldName = $(this).attr('name');
+        var $wrapper = $(this).closest('.form-group');
+        if(!errorData[fieldName]){
+          //no error
+          return;
+        }
+        
+        var $error = $('<span class="js-field-error help-block"></span>');
+        $error.html(errorData[fieldName]);
+        $wrapper.append($error);
+        $wrapper.addClass('has-error');
+      });
+    },
+    _removeFormErrors: function(){
+      var $form = this.$wrapper.find(this._selectors.newRepForm);
+      $form.find('.js-field-error').remove();
+      $form.find('.form-group').removeClass('has-error');  
+    },
+    _clearForm: function(){
+      this._removeFormErrors();
+      
+      var $form = this.$wrapper.find(this._selectors.newRepForm);
+      $form[0].reset();
+    },
+    
+    _addRow: function(repLog){
+      console.log(repLog);  
+    }
+  });
+
+  /**
+  * A "private" object
+  */
+  var Helper = function($wrapper){
+    this.$wrapper = $wrapper;
+  };
+  
+  
+  $.extend(Helper.prototype, {
+    calculateTotalWeight: function(){
+      var totalWeight = 0;
+      this.$wrapper.find('tbody tr').each(function(){
+        totalWeight += $(this).data('weight');
+      });  
+      return totalWeight;
+    }
+  });
+  
+})(window, jQuery);
