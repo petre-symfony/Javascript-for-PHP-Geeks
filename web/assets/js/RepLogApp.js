4c5d213dab14a1a4e754b0f66981617a3a1351b9
petrero <petrero@gmail.com>
2017-03-31 21:55:23 +0300

    In practice, this means we can call .done() on our .log(data). And guess what? We can just chain more handlers off of this one: add another .done() that looks the same. Print a message - another handler - and also console.log(data) again.

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -90,6 +90,12 @@
           var errorData = JSON.parse(jqXHR.responseText);
           self._mapErrorsToForm(errorData.errors);
         }
+      }).done(function(data){
+        console.log('I am successful');
+        console.log(data);
+      }).done(function(data){
+        console.log('another handler');
+        console.log(data);
       });
     },
     _mapErrorsToForm: function(errorData){
